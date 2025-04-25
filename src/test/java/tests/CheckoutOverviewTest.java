95f0d2cf3fbcd4f413509440ba115273e6949f1b
Kofanskih <kofanskih08@gmail.com>
2025-04-25 23:06:37 +0300

    Add CheckoutOverview page and test

---
+++ src/test/java/tests/CheckoutOverviewTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import com.saucedemo.pageModels.UserCheckoutPageModel;
+import com.saucedemo.pageModels.UserLoginPageModel;
+import com.saucedemo.pages.*;
+import com.saucedemo.utils.ConfigurateBrowserSettings;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CheckoutOverviewTest {
+    private String checkoutOverviewHeader = "Checkout: Overview";
+    private String productsHeader = "Products";
+
+    @BeforeClass
+    void preConditionClass(){
+        new ConfigurateBrowserSettings().setUp();
+    }
+
+    @BeforeMethod
+    void preconditionMethod(){
+        new BasePage().open();
+        new LoginPage()
+                .fillLoginDataWithUserModel(new UserLoginPageModel().userLogin());
+        new ProductsPage().userGoToCart();
+        new CartPage()
+                .userGoToCheckout();
+    }
+
+    @Test
+    public void userGoToCheckoutOverview(){
+        new CheckoutPage()
+                .fillCheckoutInfo(new UserCheckoutPageModel()
+                        .fullUserData())
+                .checkUserGoToCheckoutOverview(checkoutOverviewHeader);
+    }
+
+    @Test
+    public void userCancelCheckoutOverview(){
+        new CheckoutPage()
+                .fillCheckoutInfo(new UserCheckoutPageModel()
+                        .fullUserData())
+                .userClickCancelButton().checkHeaderOnProductsPage(productsHeader);
+    }
+}
