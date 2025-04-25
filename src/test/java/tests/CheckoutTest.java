aaa59f5859b2a65df0e41c32ad875a6e8d67310b
Kofanskih <kofanskih08@gmail.com>
2025-04-25 22:41:11 +0300

    Updated CheckoutPage, CheckoutTest

--- src/test/java/tests/CheckoutTest.java
+++ src/test/java/tests/CheckoutTest.java
@@ -1,10 +1,8 @@
 package tests;
 
+import com.saucedemo.pageModels.UserCheckoutPageModel;
 import com.saucedemo.pageModels.UserLoginPageModel;
-import com.saucedemo.pages.BasePage;
-import com.saucedemo.pages.CartPage;
-import com.saucedemo.pages.LoginPage;
-import com.saucedemo.pages.ProductsPage;
+import com.saucedemo.pages.*;
 import com.saucedemo.utils.ConfigurateBrowserSettings;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -12,6 +10,12 @@ import org.testng.annotations.Test;
 
 public class CheckoutTest {
     private String checkoutHeader = "Checkout: Your Information";
+    private String cartHeader = "Your Cart";
+    private String warningFirstName = "Error: First Name is required";
+    private String warningLastName = "Error: Last Name is required";
+    private String warningPostalCode = "Error: Postal Code is required";
+
+
     @BeforeClass
     void preConditionClass(){
         new ConfigurateBrowserSettings().setUp();
@@ -31,4 +35,55 @@ public class CheckoutTest {
                 .userGoToCheckout()
                 .checkUserOnTheCheckoutPage(checkoutHeader);
     }
+
+    @Test
+    public void userCancelCheckout(){
+        new CartPage()
+                .userGoToCheckout()
+                .userCancelCheckout()
+                .checkUserOnTheCart(cartHeader);
+    }
+
+    @Test
+    public void userLeftEmptyFields(){
+        new CartPage()
+                .userGoToCheckout()
+                .fillCheckoutInfo(new UserCheckoutPageModel().userEmptyData());
+        new CheckoutPage()
+                .checkWarningMessageAfterFillIncorrectData(warningFirstName);
+
+    }
+
+    @Test
+    public void userLeftFirstNameEmpty(){
+        new CartPage()
+                .userGoToCheckout()
+                .fillCheckoutInfo(new UserCheckoutPageModel().userDataWithoutFirstName());
+        new CheckoutPage()
+                .checkWarningMessageAfterFillIncorrectData(warningFirstName);
+
+    }
+
+    @Test
+    public void userLeftLastNameEmpty(){
+        new CartPage()
+                .userGoToCheckout()
+                .fillCheckoutInfo(new UserCheckoutPageModel().userDataWithoutLastName());
+        new CheckoutPage()
+                .checkWarningMessageAfterFillIncorrectData(warningLastName);
+
+    }
+    @Test
+    public void userLeftPostalCodeEmpty(){
+        new CartPage()
+                .userGoToCheckout()
+                .fillCheckoutInfo(new UserCheckoutPageModel().userDataWithoutPostalCode());
+        new CheckoutPage()
+                .checkWarningMessageAfterFillIncorrectData(warningPostalCode);
+
+    }
+
+
+
+
 }
